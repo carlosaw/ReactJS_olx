bd867de6c143e06c70887354a72488b3eb416a98
carlosaw <carlosfreevg@gmail.com>
2023-09-05 17:15:00 -0400

    Button styled

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React from "react";
+import { PageArea } from './styled';
+
+import { PageContainer, PageTitle } from '../../components/MainComponents';
 
 const Page = () => {
   return (
-    <div>
-      <h1>Página de Login</h1>
-    </div>
+    <PageContainer>
+      <PageTitle>Login</PageTitle>
+      <PageArea>
+        <form>
+          <label className="area">
+            <div className="area--title">E-mail</div>
+            <div className="area--input">
+              <input type="email" />
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title">Senha</div>
+            <div className="area--input">
+              <input type="password" />
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title">Lembrar Senha</div>
+            <div className="area--input--check">
+              <input type="checkbox" className="check" />
+            </div>
+          </label>
+          <label className="area">
+            <div className="area--title"></div>
+            <div className="area--input">
+              <button>Fazer Login</button>
+            </div>
+          </label>
+        </form>
+      </PageArea>
+    </PageContainer>
   );
 }
 
