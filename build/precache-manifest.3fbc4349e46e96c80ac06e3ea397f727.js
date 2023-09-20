69cfb6d1d9a0fd245187de373c851cec51902a6c
carlosaw <carlosfreevg@gmail.com>
2023-09-20 16:04:10 -0400

    build

---
+++ build/precache-manifest.3fbc4349e46e96c80ac06e3ea397f727.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4494e0113004cbcba61c50995c70006e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26d9afb8df25e1de490e",
+    "url": "/static/css/main.7987330d.chunk.css"
+  },
+  {
+    "revision": "46bc2c22b123bf802786",
+    "url": "/static/js/2.8d4bc180.chunk.js"
+  },
+  {
+    "revision": "26d9afb8df25e1de490e",
+    "url": "/static/js/main.debcce6c.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  },
+  {
+    "revision": "aba492a49cf1f1f087fb1a772821b11b",
+    "url": "/static/media/arrowNext.aba492a4.png"
+  },
+  {
+    "revision": "e2e48230a831bd2a35fa560469b52b86",
+    "url": "/static/media/arrowPrev.e2e48230.png"
+  }
+]);
